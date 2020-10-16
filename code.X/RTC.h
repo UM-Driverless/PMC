f741f8f08cc75ca3e76fea8d3051fa338a3943ce
panze <panze@DESKTOP-9LH90U2>
2020-10-16 18:08:47 +0200

    Creacion de source RTC

---
+++ code.X/RTC.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   RTC.h
+ * Author: 93Urbano
+ *
+ * Created on 16 de octubre de 2020, 18:06
+ */
+
+#ifndef RTC_H
+#define	RTC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RTC_H */
+
