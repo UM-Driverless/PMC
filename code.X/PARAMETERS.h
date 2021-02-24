3d950502d042eee23f7abd9c3ceed3282e62841d
heqro <heqro@heqro-herobookpro>
2021-02-24 21:29:55 +0100

    añadido formato de datos del CAN

--- code.X/PARAMETERS.h
+++ code.X/PARAMETERS.h
@@ -20,7 +20,28 @@ extern "C" {
 #define N_CAN 20
 #define FTQ = N_CAN * BAUDRATE
 #define BRP_VAL		((FCY/(2*FTQ))-1)
-
+        // Estructura de datos del mensaje CAN
+typedef struct {
+    // campo de datos representado por 8 bytes
+    unsigned char datos[8];
+    // identificador que consta de 11 bits
+    unsigned long identificador;
+    
+    
+    /* Dado que sabemos que utilizamos formato estándar, podemos dejar
+     de considerar todas las comprobaciones relativas a si el mensaje tiene
+     * un formato de datos u otro.
+     * 
+     * Dejo las comprobaciones aquí abajo, por si acaso: */
+    
+    // identifica si es un mensaje en formato estándar o extendido
+    // unsigned char estandar_o_extendido;
+    
+    // identificador de si contamos con mensaje de datos o de RTR
+    // Como contamos con un formato de datos básico, esto siempre está a cero
+    // unsigned char RTR_o_datos;
+    
+}msgCAN;
 
 #ifdef	__cplusplus
 }
