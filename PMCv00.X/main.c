3d8a5ff0fa65d655444ce96fc541d333348d8658
93Urbano <davidjimenezurbano@gmail.com>
2022-07-15 21:43:32 +0200

    Mensaje CAN RES probado

--- PMCv00.X/main.c
+++ PMCv00.X/main.c
@@ -227,7 +227,7 @@ void RESAnalyse (void)
     if ( ucSendRESOperative == TRUE )
     {
         //para reset
-        ecan1WriteMessage(RES_OPERATIONALMODE, DataLength_2, 0x80, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+        //ecan1WriteMessage(RES_OPERATIONALMODE, DataLength_2, 0x80, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
         //para iniciar
         ecan1WriteMessage(RES_OPERATIONALMODE, DataLength_2, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
         ucSendRESOperative = FALSE;
